96579b03da5c584efd5e46046c81bc84bd430d88
Target Architecture <targetarchitecture@users.noreply.github.com>
2021-02-26 12:26:45 +0000

    Update credentials template.h

--- src/include/credentials template.h
+++ src/include/credentials template.h
@@ -2,12 +2,12 @@
 auto WIFI_SSID = "the robot network";
 auto WIFI_PASSWORD = "************************************";
 
-auto MDNS_HOSTNAME = "ps2controller";
+auto MQTT_SERVER = "robotmqtt";
 
-auto MQTT_SERVER = "******************************************.cloud.shiftr.io";
 auto MQTT_CLIENTID = "PS2controller";
-auto MQTT_USERNAME = "targetarchitecture";
-auto MQTT_KEY = "******************************************";
+auto MQTT_USERNAME = "";
+auto MQTT_KEY = "";
+
 auto MQTT_TOPIC = "PS2/state";
 auto MQTT_IP_TOPIC = "PS2/IP";
 auto MQTT_DIAL_TOPIC = "PS2/dial";
