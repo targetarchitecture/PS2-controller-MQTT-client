a02467aba2c0d244799f896ee0ac70e23568bb71
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-09-30 11:18:41 +0100

    more topics

--- src/include/topics.h
+++ src/include/topics.h
@@ -1,16 +1,21 @@
 #pragma once
 
-auto MQTT_LEFT_TOPIC = "ps2/sticks/left";
-auto MQTT_RIGHT_TOPIC = "ps2/sticks/right";
-auto MQTT_BUTTON_TOPIC = "ps2/buttons";
-//auto MQTT_BUTTON_MINIMISED_TOPIC = "ps2/buttons/min";
+auto MQTT_LEFT_X_TOPIC = "gamepads/ps2/1/sticks/left/x";
+auto MQTT_RIGHT_X_TOPIC = "gamepads/ps2/1/sticks/right/x";
+auto MQTT_LEFT_Y_TOPIC = "gamepads/ps2/1/sticks/left/y";
+auto MQTT_RIGHT_Y_TOPIC = "gamepads/ps2/1/sticks/right/y";
 
-auto MQTT_RUMBLE_TOPIC = "ps2/rumble";
+auto MQTT_LEFT_X_RAW_TOPIC = "gamepads/ps2/1/sticks/left/xraw";
+auto MQTT_RIGHT_X_RAW_TOPIC = "gamepads/ps2/1/sticks/right/xraw";
+auto MQTT_LEFT_Y_RAW_TOPIC = "gamepads/ps2/1/sticks/left/yraw";
+auto MQTT_RIGHT_Y_RAW_TOPIC = "gamepads/ps2/1/sticks/right/yraw";
 
-//auto MQTT_CONNECTED_TOPIC = "PS2/connected";
+auto MQTT_BUTTON_TOPIC = "gamepads/ps2/1/buttons";
 
-auto MQTT_INFO_TOPIC = "ps2/info";
-auto MQTT_IP_TOPIC = "ps2/ip";
-auto MQTT_DIAL_TOPIC = "ps2/dial";
-auto MQTT_ERROR_TOPIC = "ps2/error";
+auto MQTT_RUMBLE_TOPIC = "gamepads/ps2/1/rumble";
+
+auto MQTT_INFO_TOPIC = "gamepads/ps2/1/info";
+auto MQTT_IP_TOPIC = "gamepads/ps2/1/ip";
+auto MQTT_DIAL_TOPIC = "gamepads/ps2/1/dial";
+auto MQTT_ERROR_TOPIC = "gamepads/ps2/1/error";
 
